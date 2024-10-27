d3e79253584b9af586e966ad2b2d8029fd0da396
jiwon <tlsxp13@gmail.com>
2024-10-27 23:04:43 +0900

    20241027 jiwon

---
+++ 10 October/1027/최대힙_11279.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+heap = []
+
+N = int(input())
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(-heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, -x)
